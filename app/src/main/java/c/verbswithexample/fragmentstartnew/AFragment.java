9d946fb999e1c3520c159d1e6666931df34dc4e5
a4anddev <a4anddev@gmail.com>
2018-08-02 16:35:16 +0530

    fragment life cycle methods all add fragment button also include all method

--- app/src/main/java/c/verbswithexample/fragmentstartnew/AFragment.java
+++ app/src/main/java/c/verbswithexample/fragmentstartnew/AFragment.java
@@ -1,8 +1,11 @@
 package c.verbswithexample.fragmentstartnew;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +15,10 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class AFragment extends Fragment {
+    private static final String COMMON_TAG = "CombinedLifeCycle";
+    private static final String FRAGMENT_NAME = AFragment.class.getSimpleName();
 
+    private static final String TAG = COMMON_TAG;
 
     public AFragment() {
         // Required empty public constructor
@@ -20,10 +26,71 @@ public class AFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG, FRAGMENT_NAME +" onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, FRAGMENT_NAME +" onCreate");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, FRAGMENT_NAME +" onCreateView");
+        return inflater.inflate(R.layout.fragment_a,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.i(TAG, FRAGMENT_NAME +" onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(TAG, FRAGMENT_NAME +" onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, FRAGMENT_NAME +" onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i(TAG, FRAGMENT_NAME +" onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i(TAG, FRAGMENT_NAME +" onStop");
+        super.onStop();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        Log.i(TAG, FRAGMENT_NAME +" onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, FRAGMENT_NAME +" onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i(TAG, FRAGMENT_NAME +" onDetach");
+        super.onDetach();
     }
 
 }
--- app/src/main/java/c/verbswithexample/fragmentstartnew/MainActivity.java
+++ app/src/main/java/c/verbswithexample/fragmentstartnew/MainActivity.java
@@ -4,8 +4,15 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String COMMON_TAG = "CombinedLifeCycle";
+    private static final String ACTIVITY_NAME = MainActivity.class.getSimpleName();
+    private static final String TAG = COMMON_TAG;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +38,15 @@ public class MainActivity extends AppCompatActivity {
         * android.support.v7.app -> appCompatActivity = getSupportFragmentManager this is support both v4 and v7
         * */
 
-
+/*
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         AFragment aFragment = new AFragment();
         transaction.add(R.id.fragA, aFragment);
-        transaction.commit();
+
+        // close fragment from back button
+        transaction.addToBackStack("fragmentStack");
+        transaction.commit();*/
 
 
         /*
@@ -66,6 +76,56 @@ public class MainActivity extends AppCompatActivity {
         *
         * */
 
+
+
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, ACTIVITY_NAME+" onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, ACTIVITY_NAME+" onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, ACTIVITY_NAME+" onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, ACTIVITY_NAME+" onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, ACTIVITY_NAME+" onDestroy");
+    }
+
+    private void addFragment(){
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        AFragment aFragment = new AFragment();
+        transaction.add(R.id.fragA, aFragment);
+
+        // close fragment from back button
+        transaction.addToBackStack("fragmentStack");
+        transaction.commit();
+    }
+
+    public void AddFragmentButton(View view) {
+
+        addFragment();
+    }
 }
