0f8daafae34f44bb91c116f3c09d0b32fd31fe48
a4anddev <a4anddev@gmail.com>
2018-08-02 14:19:38 +0530

    dynamic fragment add view mainActivity comment code same old way little way change

--- app/src/main/java/c/verbswithexample/fragmentstartnew/MainActivity.java
+++ app/src/main/java/c/verbswithexample/fragmentstartnew/MainActivity.java
@@ -1,5 +1,7 @@
 package c.verbswithexample.fragmentstartnew;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
         *   which activity you want to show in which you have to add xml <fragemnt> tag with name and id both are important
         *
         * */
+
+
+        /*
+        * dyanamic way to add fragment you have to follow same static way only you have to change activity xml file which file
+        * you wanna show xml file change framelayout with id after mainactivity java you have to change
+        * fragmentmanger way use
+        *
+        * */
+
+        /*
+        * android.support.v4.app -> fragmentactivity = getFragmentManager
+        * android.support.v7.app -> appCompatActivity = getSupportFragmentManager this is support both v4 and v7
+        * */
+
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        AFragment aFragment = new AFragment();
+        transaction.add(R.id.fragA, aFragment);
+        transaction.commit();
+
     }
 }
